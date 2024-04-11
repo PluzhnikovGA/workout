f7b29e6cc623d47de828b9695e2900a8587c12e6
Pluzhnikov Grigorii <pluzhnikovga@gmail.com>
2024-04-11 18:45:52 +0400

    Connected prisma and created first table "Users"

--- workout-backend/server.js
+++ workout-backend/server.js
@@ -1,16 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
 import 'colors';
 import authRoutes from './app/auth/auth.routes.js';
+import { prisma } from './app/prisma.js';
+
+dotenv.config();
 
 const app = express();
 
 async function main() {
-  // if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+  if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
   app.use(express.json());
   app.use('/api/auth', authRoutes);
 
-  const PORT = 6000;
+  const PORT = process.env.PORT || 6000;
 
   app.listen(
     PORT,
@@ -21,4 +26,12 @@ async function main() {
   )
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
