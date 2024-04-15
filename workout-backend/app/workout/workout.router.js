730fa8e49003e664eb856c5bb5aa6a9156c228f3
Pluzhnikov Grigorii <pluzhnikovga@gmail.com>
2024-04-15 21:22:33 +0400

    Add function for createNewWorkoutLog

--- workout-backend/app/workout/workout.router.js
+++ workout-backend/app/workout/workout.router.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { protect } from '../middleware/auth.middleware.js';
 import { createNewWorkout, getWorkout, deleteWorkout, updateWorkout, getWorkouts } from './workout.controller.js';
+import { createNewWorkoutLog } from './log/workoutLog.controller.js';
 
 const router = express.Router();
 
 router.route('/').get(protect, getWorkouts).post(protect, createNewWorkout);
+
 router.route('/:id').get(protect, getWorkout).put(protect, updateWorkout).delete(protect, deleteWorkout);
+
+router.route('/log/:id').post(protect, createNewWorkoutLog);
 
 export default router;
