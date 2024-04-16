2f5b83a3ee783c32aaf6b5fadca0d60877a81ee2
Pluzhnikov Grigorii <pluzhnikovga@gmail.com>
2024-04-16 22:06:56 +0400

    Add statistic for user

--- workout-backend/app/user/user.controller.js
+++ workout-backend/app/user/user.controller.js
@@ -1,6 +1,7 @@
 import { prisma } from "../prisma.js";
 import asyncHandler from "express-async-handler";
 import { UserFields } from "../utils/user.utils.js";
+import { calculateMinutes } from "../utils/calculateMinutes.utils.js";
 
 // @desc Get user profile
 // @route GET /api/user/profile
@@ -13,5 +14,47 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     select: UserFields,
   });
 
-  res.json(user);
+  const countExerciseTimesCompleted = await prisma.exerciseLog.count({
+    where: {
+      userId: req.user.id,
+      isCompleted: true,
+    }
+  });
+
+  const kgs = await prisma.exerciseTime.aggregate({
+    where: {
+      exerciseLog: {
+        userId: req.user.id,
+      },
+      isCompleted: true,
+    },
+    _sum: {
+      weight: true
+    }
+  });
+
+  const workouts = await prisma.workoutLog.count({
+    where: {
+      userId: req.user.id,
+      isCompleted: true,
+    },
+  });
+
+  res.json({
+    user,
+    statistic: [
+      {
+        label: "Minutes",
+        value: calculateMinutes(countExerciseTimesCompleted) || 0,
+      },
+      {
+        label: "Workouts",
+        value: workouts,
+      },
+      {
+        label: "Kgs",
+        value: kgs._sum.weight || 0,
+      },
+    ]
+  });
 });
